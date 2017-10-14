8454f204fefbfcce69f745d7d3aa582a8e5b12db
Kazuki Ota <kaota@microsoft.com>
2017-10-14 15:36:52 +0900

    init

---
+++ TechSummit2017Weather/TechSummit2017WeatherPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TechSummit2017Weather
+{
+    public partial class TechSummit2017WeatherPage : ContentPage
+    {
+        public TechSummit2017WeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
